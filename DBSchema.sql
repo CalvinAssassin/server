d5b574c764a8a1dcd86537ef1be2e41f54bfb6ca
Paige Brinks <Paige Brinks>
2016-11-09 22:36:07 -0500

    updated DBSchema

--- DBSchema.sql
+++ DBSchema.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS Player CASCADE;
 DROP TABLE IF EXISTS PlayerGame CASCADE;
 DROP TABLE IF EXISTS Game CASCADE;
 DROP TABLE IF EXISTS PlayerMatchUp CASCADE;
+DROP TABLE IF EXISTS PlayerLocation CASCADE;
 
 
 
@@ -23,11 +24,11 @@ CREATE TABLE Game (
 	inPlay boolean,
 	creatorID integer REFERENCES Player(playerID),
 	startDate date, 
-	round int 
+	round integer 
 	);
 
 CREATE TABLE PlayerGame (
-	-- playerGameID integer PRIMARY KEY,
+	playerGameID integer PRIMARY KEY,
 	gameID integer REFERENCES Game(gameID), 
 	playerID integer REFERENCES Player(playerID),
 	stillAlive boolean
@@ -35,17 +36,27 @@ CREATE TABLE PlayerGame (
 
 
 CREATE TABLE PlayerMatchUp (
+	playerMatchID integer PRIMARY KEY,
 	gameID integer REFERENCES Game(gameID),
 	assassinID integer REFERENCES Player(playerID),
 	targetID integer REFERENCES Player(playerID), -- target
 	targetAlive boolean
 	);
 
-	-- Allow users to select data from the tables.
+CREATE TABLE PlayerLocation (
+	playerID integer REFERENCES Player(playerID),
+	gameID integer REFERENCES Game(gameID),
+	latitude float,
+	longitude float,
+	time timestamp
+	);
+
+-- Allow users to select data from the tables.
 GRANT SELECT ON Game TO PUBLIC;
 GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON PlayerGame TO PUBLIC;
 GRANT SELECT ON PlayerMatchUp TO PUBLIC;
+GRANT SELECT ON PlayerLocation TO PUBLIC;
 
 INSERT INTO Player VALUES (1, 'plb7', '123password', 'Paige', 'Brinks', 'senior', 'computer science');
 INSERT INTO Player VALUES (2, 'jjh35', 'abc123', 'Jesse', 'Hulse', 'senior', 'computer science');
@@ -64,11 +75,13 @@ INSERT INTO PlayerMatchUp VALUES (1, 1, 2, true);
 INSERT INTO PlayerMatchUp VALUES (1, 2, 3, true);
 INSERT INTO PlayerMatchUp VALUES (1, 3, 1, true);
 
-
+INSERT INTO PlayerLocation VALUES (1, 1, 42.9634, 85.6681);
+INSERT INTO PlayerLocation VALUES (2, 1, 43.1254, 84.9874);
+INSERT INTO PlayerLocation VALUES (3, 1, 41.6512, 87.2231);
 
 -- get target of player1
- SELECT targetID FROM PlayerMatchUp
- WHERE assassinID = 1;
+SELECT targetID FROM PlayerMatchUp
+WHERE assassinID = 1;
 
 -- get game rankings - everyone who is still in game 1
 SELECT userName FROM Player, PlayerGame
